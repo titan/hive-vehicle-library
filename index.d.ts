f412a97b0096e1465b61f8cbcd72cc2cd9b393cd
Titan <howay.tan@gmail.com>
2017-04-20 15:08:58 +0800

    Refactor vehicle model

--- index.d.ts
+++ index.d.ts
@@ -1,7 +1,22 @@
 export interface VehicleModel {
     code: string;
     source: number;
-    data: Map<any, any>;
+    vehicle_code: string;
+    vehicle_name: string;
+    brand_name: string;
+    family_name: string;
+    body_type: string;
+    engine_desc: string;
+    gearbox_name: string;
+    year_pattern: string;
+    group_name: string;
+    cfg_level: string;
+    purchase_price: string;
+    purchase_price_tax: string;
+    seat: string;
+    effluent_standard: string;
+    pl: string;
+    driven_type: string;
 }
 export interface Vehicle {
     id: string;
