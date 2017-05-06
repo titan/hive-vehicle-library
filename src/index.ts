dfebdf1ed9c980c9b92d12b55cbc5fb837dbc8c8
Titan <howay.tan@gmail.com>
2017-05-06 16:05:45 +0800

    Add driving view and issue date

--- src/index.ts
+++ src/index.ts
@@ -21,16 +21,18 @@ export interface VehicleModel {
 
 export interface Vehicle {
   id: string;
-  vin: string;
-  license_no: string;
-  engine_no: string;
-  register_date: Date;
-  model?: VehicleModel;
-  is_transfer: boolean;
-  receipt_no: string;
-  receipt_date: Date;
-  last_insurance_company: string;
-  insurance_due_date: Date;
-  accident_status: number;
+  vin?: string;
+  license_no?: string;
+  engine_no?: string;
+  register_date?: Date;
+  model: VehicleModel;
+  is_transfer?: boolean;
+  receipt_no?: string;
+  receipt_date?: Date;
+  last_insurance_company?: string;
+  insurance_due_date?: Date;
+  accident_status?: number;
   ncd?: Map<[Date, Date], number>;
+  issue_date?: Date;
+  driving_view?: string;
 };
